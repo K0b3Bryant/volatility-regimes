4aeff16fc264d7117741d9ffb92ab183cdfb1327
Marco <sejereje1011@gmail.com>
2025-01-08 00:59:37 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Configuration File
+
+# File paths for datasets
+data_files = {
+    'spx': '/content/SPX.csv',
+    'vix': '/content/VIX.csv',
+    'features_5d': '/content/features_5d.csv',
+    'features_ta': '/content/features_ta.csv'
+}
+
+# Global settings
+random_state = 42
